1d4885828caad59de8fa04b8a46677592e47d536
Brandon W <bwaggone@umich.edu>
2017-03-30 16:53:12 -0400

    More testing

--- mpu9250_arduino/mpu9250_arduino.ino
+++ mpu9250_arduino/mpu9250_arduino.ino
@@ -77,9 +77,9 @@ void setup()
  
  
 long int cpt=0;
-const char* filename = "datalogger.txt";
+//const char* filename = "datalogger.txt";
 //auto fp = fopen(filename, "w");
-int32_t xvel = 0, yvel = 0, xpos = 0, ypos = 0;
+float xvel = 0, yvel = 0, xpos = 0, ypos = 0;
 int xdeg = 0, ydeg = 0, zdeg = 0;
 Quat testQuat;
 
@@ -130,50 +130,18 @@ void loop()
   int16_t ax=(-(Buf[0]<<8 | Buf[1]));
   int16_t ay=(-(Buf[2]<<8 | Buf[3]));
   int16_t az=(Buf[4]<<8 | Buf[5]);
-  int32_t ax32 = 0;
-  int32_t ay32 = 0;
-  int32_t az32 = 0;
-
-  /*if(ax > 1400){
-    ax32 = ax;
-    ay32 = ay;
-    az32 = az;
-  }
-
-  if(ax < -1200){
-    ax32 = ax;
-    ay32 = ay;
-    az32 = az;
-  }*/
-
-
 
   axg = (float(ax) / (8192));
   ayg= (float(ay) / (8192));
   azg = (float(az) / (8192));
-
-  //if(ax32 < 0){
-  //  Serial.print("negative motion!\n");
-  //}
  
   // Gyroscope
   int16_t gx=-(Buf[8]<<8 | Buf[9]);
   int16_t gy=-(Buf[10]<<8 | Buf[11]);
   int16_t gz=Buf[12]<<8 | Buf[13];
- /*
-    // Display values
-  int32_t del_xvel = ax * (dt*1e-6);
-  xvel += del_xvel;
-  int32_t del_yvel = ay * dt *1e-6;
-  yvel += del_yvel;
+
   
-  int32_t del_xpos = xvel * dt*1e-6;
-  xpos += del_xpos;
-  int32_t del_ypos = yvel *dt*1e-6;
-  ypos += del_ypos;
-  */
-  
-  
+ // This section of code correctly positions and update the unit quaternion for orientation  
   
 
     GyroVec  = Vector((gx / 180)* PI, (gy / 180)* PI, (gz / 180)* PI);  // move gyro data to vector structure
@@ -191,42 +159,33 @@ void loop()
 
     AttitudeEstimateQuat = Mul(incrementalRotation, AttitudeEstimateQuat);  // quaternion integration (rotation composting through multiplication)
 
-/*if(cpt == 1){
-  Serial.print (incrementalRotation.x); 
-    Serial.print ("\t");
-    Serial.print (AttitudeEstimateQuat.y);
-    Serial.print ("\t");
-    Serial.print (GyroVec.z);
-    Serial.print ("\t");
-  
-}*/
-  
-/*
-  double ysqr = AttitudeEstimateQuat.y * AttitudeEstimateQuat.y;
-  double roll, pitch, yaw;
-  // roll (x-axis rotation)
-  double t0 = +2.0 * (AttitudeEstimateQuat.w * AttitudeEstimateQuat.x + AttitudeEstimateQuat.y * AttitudeEstimateQuat.z);
-  double t1 = +1.0 - 2.0 * (AttitudeEstimateQuat.x * AttitudeEstimateQuat.x + ysqr);
-  roll = atan2(t0, t1);
-
-  // pitch (y-axis rotation)
-  double t2 = +2.0 * (AttitudeEstimateQuat.w * AttitudeEstimateQuat.y - AttitudeEstimateQuat.z * AttitudeEstimateQuat.x);
-  t2 = t2 > 1.0 ? 1.0 : t2;
-  t2 = t2 < -1.0 ? -1.0 : t2;
-  pitch = asin(t2);
-
-  // yaw (z-axis rotation)
-  double t3 = +2.0 * (AttitudeEstimateQuat.w * AttitudeEstimateQuat.z + AttitudeEstimateQuat.x * AttitudeEstimateQuat.y);
-  double t4 = +1.0 - 2.0 * (ysqr + AttitudeEstimateQuat.z * AttitudeEstimateQuat.z);  
-  yaw = atan2(t3, t4);
-  */
   
   // Accelerometer
 
   //Quat changeQuat = Quaternion(Vector((gx / 180)* PI, (gy / 180)* PI, (gz / 180)* PI), dt);
   //testQuat = Mul(changeQuat, testQuat);
   auto testRotate = Rotate(AttitudeEstimateQuat, Vector(axg,ayg,azg));
-  //testRotate.z += -1;
+  
+  
+   
+    // Display values
+    /*
+  float del_xvel = axg * (dt*1e-6);
+  xvel += del_xvel;
+  float del_yvel = ayg * (dt*1e-6);
+  yvel += del_yvel;
+
+  float del_xpos = xvel * (dt*1e-6);
+  xpos += del_xpos;
+  float del_ypos = yvel * (dt*1e-6);
+  ypos += del_ypos;
+  */
+  //int32_t del_xpos = xvel * dt*1e-6;
+  //xpos += del_xpos;
+  //int32_t del_ypos = yvel *dt*1e-6;
+  //ypos += del_ypos;
+  
+  
   if(cpt % 50 == 0){
     //Serial.print (ax); 
     //Serial.print ("\t");
@@ -234,12 +193,12 @@ void loop()
     //Serial.print ("\t");
     //Serial.print (az);
     //Serial.print ("\t");
-    Serial.print (testRotate.x); 
+    Serial.print (axg); 
     Serial.print ("\t");
-    Serial.print (testRotate.y);
+    Serial.print (ayg);
     Serial.print ("\t");
-    Serial.print (abs(testRotate.z) - 1);
-    Serial.print ("\t");
+    //Serial.print (abs(testRotate.z) - 1);
+    //Serial.print ("\t");
     
     //Serial.print (xpos / SHIFT,DEC);
 
@@ -262,15 +221,6 @@ if(cpt < 1000){
   
 }*/
 
- 
-  // Gyroscope
-  //Serial.print (gx,DEC); 
-  //Serial.print ("\t");
-  //Serial.print (gy,DEC);
-  //Serial.print ("\t");
-  //Serial.print (gz,DEC);  
-  //Serial.print ("\t");
- 
  
   // _____________________
   // :::  Magnetometer ::: 
@@ -306,6 +256,26 @@ if(cpt < 1000){
   Serial.print (mz-700,DEC);  
   Serial.print ("\t");
  */
+
+ /*
+  double ysqr = AttitudeEstimateQuat.y * AttitudeEstimateQuat.y;
+  double roll, pitch, yaw;
+  // roll (x-axis rotation)
+  double t0 = +2.0 * (AttitudeEstimateQuat.w * AttitudeEstimateQuat.x + AttitudeEstimateQuat.y * AttitudeEstimateQuat.z);
+  double t1 = +1.0 - 2.0 * (AttitudeEstimateQuat.x * AttitudeEstimateQuat.x + ysqr);
+  roll = atan2(t0, t1);
+
+  // pitch (y-axis rotation)
+  double t2 = +2.0 * (AttitudeEstimateQuat.w * AttitudeEstimateQuat.y - AttitudeEstimateQuat.z * AttitudeEstimateQuat.x);
+  t2 = t2 > 1.0 ? 1.0 : t2;
+  t2 = t2 < -1.0 ? -1.0 : t2;
+  pitch = asin(t2);
+
+  // yaw (z-axis rotation)
+  double t3 = +2.0 * (AttitudeEstimateQuat.w * AttitudeEstimateQuat.z + AttitudeEstimateQuat.x * AttitudeEstimateQuat.y);
+  double t4 = +1.0 - 2.0 * (ysqr + AttitudeEstimateQuat.z * AttitudeEstimateQuat.z);  
+  yaw = atan2(t3, t4);
+  */
 
  //if(cpt == 1000){
  // fclose(fp);
